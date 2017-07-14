7834dbd9529e63b43e5679965e8ccde409fd7134
Wes <wes.coffay@gmail.com>
2017-07-14 19:17:31 -0400

    added style.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentBox from './CommentBox'
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+//im not sure what service worker does
+import registerServiceWorker from './registerServiceWorker';
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('root')
+);
 registerServiceWorker();
